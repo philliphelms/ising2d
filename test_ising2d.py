0e22fbf1cfb2de34fd3f6bb7bd018ad6c3b972d4
phillip-helms <phelms@caltech.edu>
2018-07-06 15:11:15 -0400

    add spin flip

--- test_ising2d.py
+++ test_ising2d.py
@@ -6,11 +6,33 @@ class ising_2d_test(unittest.TestCase):
         ising=ising2d()
         self.assertEqual(ising.lattice_size,ising.lattice.shape[0])
         self.assertEqual(ising.lattice_size,ising.lattice.shape[1])
-#    def test_initialization(self) :
-#        ising=ising2d()
-#        ising2d.initialize()
-        
-#        self.lattice
+
+    def test_initialization(self) :
+        ising=ising2d()
+        ising.initialize_guess()
+        for i in range(ising.lattice_size) :
+            for j in range(ising.lattice_size) :
+                spin=ising.lattice[i,j]
+                self.assertEqual(((spin==0) or (spin==1)),True)
+
+    def test_flip_single_spin(self):
+        ising = ising2d()
+        ising.initialize_guess()
+        # Select random x and y coordinate
+        indx = np.random.randint(ising.lattice_size)
+        indy = np.random.randint(ising.lattice_size)
+        # Check initial state
+        initial_state = ising.lattice[indx,indy]
+        # Flip Spin
+        ising.flip_spin(indx,indy)
+        # Check final state
+        final_state = ising.lattice[indx,indy]
+        if initial_state is 1:
+            self.assertEqual(final_state,0)
+        elif initial_state is 0:
+            self.assertEqual(final_state,1)
+
+                
 if __name__ == "__main__":
     unittest.main()
 
